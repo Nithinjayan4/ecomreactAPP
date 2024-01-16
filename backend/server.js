9779764cc11e49ae130216e09eae09eb33cdc25d
Nithin S J <77042006+Nithinjayan4@users.noreply.github.com>
2024-01-16 22:59:03 +0530

    cors added

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,14 @@ import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cors from 'cors';
 
 const port = process.env.PORT || 5000;
+
+app.use(cors(
+  { origin: 'https://kaleidoscopic-crepe-be647e.netlify.app', credentials: true }
+));
+
 
 connectDB();
 
